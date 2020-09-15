b5bc8d9119855f77dc2bc6d8cd0aae3324fd1049
Sérgio Campos <srabelocampos@gmail.com>
2020-09-15 12:10:06 -0300

    Estrutura da aplicação foi criada. Ainda falta fazer persistência com o banco.

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+
+const appointmentsRouter = Router();
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'There is already an appointment on this time / date' });
+  }
+
+  const appointment = new Appointment(provider, parsedDate);
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-// src/server.ts
 import routes from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.get('/', (req, res) => {
   res.json({ Message: 'Hello World!' });
