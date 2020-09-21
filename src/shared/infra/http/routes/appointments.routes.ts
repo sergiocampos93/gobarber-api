b072b9955f2e2d92c69ee798797da985d5d264a4
Sérgio Campos <srabelocampos@gmail.com>
2020-09-21 00:13:11 -0300

    Separaçao da estrutura da aplicaçao em modulos e camada de infra.

--- src/shared/infra/http/routes/appointments.routes.ts
+++ src/shared/infra/http/routes/appointments.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
-import AppointmentsRepository from '../repositories/AppointmentRepository';
+import AppointmentsRepository from '../../../../modules/appointments/repositories/AppointmentRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
