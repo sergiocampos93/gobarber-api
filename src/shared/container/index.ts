79b27c861db9e0ac72df7d52072c06fafa09dbc5
Sérgio Campos <srabelocampos@gmail.com>
2020-09-28 16:21:08 -0300

    Criaçao de teste unitario, usando Jest, para validar a criaçao de appointments.

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,7 @@
 import { container } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
