9d81fdac5e13f612e72e821a017732fee0fefce1
Sérgio Campos <srabelocampos@gmail.com>
2020-10-07 21:35:55 -0300

    Teste para verficar a funcionalidade de resetar a senha do usuario foi implementado.

---
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -0,0 +1,39 @@
+// import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokensRepository';
+import ResetPasswordService from './ResetPasswordService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeUserTokensRepository: FakeUserTokensRepository;
+let resetPassword: ResetPasswordService;
+
+describe('PasswordRecoverService', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeUserTokensRepository = new FakeUserTokensRepository();
+
+    resetPassword = new ResetPasswordService(
+      fakeUsersRepository,
+      fakeUserTokensRepository,
+    );
+  });
+
+  it('should be able to reset the password', async () => {
+    let user = await fakeUsersRepository.create({
+      email: 'john@example.com',
+      name: 'John',
+      password: '123456',
+    });
+
+    const { token } = await fakeUserTokensRepository.generate(user.id);
+
+    await resetPassword.execute({
+      password: '123123',
+      token,
+    });
+
+    user = await fakeUsersRepository.findById(user.id);
+
+    expect(user.password).toBe('123123');
+  });
+});
